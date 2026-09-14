12005f63b852fa02bc8b05cd08b9b3112cae37e1
O.Nasri <omid.nasri@outlook.com>
2026-09-14 22:02:15 +0330

    refactor: update localization keys in thumbnail views

--- Gateway.cs
+++ Gateway.cs
@@ -21,6 +21,8 @@ namespace Summary.SMS.Gateway
         {
             public const string SOfSMSGateway = "درگاه پیامک سیم کارت";
             public const string DOfSMSGateway = "مجموعه ای از رخداد و تسک‌ها جهت ارسال و دریافت پیامک با درگاه پیامک از طریق سیم کارت.";
+            public const string SOfSendMessage = "ارسال پیامک از طریق درگاه سیم کارت";
+            public const string DOfSendMessage = "فعالیتی جهت ارسال پیامک از طریق درگاه سیم کارت با عبور از لیست سیاه و با قابلیت انتخاب سیم کارت.";
         }
     }
 }
